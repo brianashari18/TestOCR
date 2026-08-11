b21267b684072f40d97269ab10950ea5063c79ff
Brian Anashari <brianashari18@gmail.com>
2026-08-11 08:05:08 +0700

    Initial Commit

---
+++ TestOCR/TestOCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestOCRApp.swift
+//  TestOCR
+//
+//  Created by Brian Anashari on 11/08/26.
+//
+
+import SwiftUI
+
+@main
+struct TestOCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
